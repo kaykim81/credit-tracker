566f387692474306661a7582195e50809ac7c812
kaykim81 <kaykim81@gmail.com>
2026-04-04 06:35:51 -0500

    Delete rebuild.sh

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker compose down
-docker rmi credit-tracker-app:latest
-docker build --no-cache -t credit-tracker-app:latest https://github.com/kaykim81/credit-tracker.git#main && \
-docker compose up -d
-docker compose logs -f app
